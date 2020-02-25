e50eac6ee0cb32566c80567ffa95f7589169c79a
cherryApp <j.cserko@gmail.com>
2020-02-25 11:52:15 +0100

    Done

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+import 'package:cherry_title/states/RandomWordState.dart';
+
+void main() => runApp(MyApp());
+
+/// This Widget is the main application widget.
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // final wordPair = WordPair.random();
+    return MaterialApp(
+      title: 'Startup Name Genarator',
+      home: RandomWords(),
+      color: Colors.amberAccent,
+    );
+  }
+}
